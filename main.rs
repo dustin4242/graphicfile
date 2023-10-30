68202d05568ab088614c770b908cbf537854d77d
dustin4242 <thegameexposer@gmail.com>
2023-10-30 09:57:03 -0500

    working on color graphic files

--- main.rs
+++ main.rs
@@ -12,24 +12,40 @@ fn main() {
 fn str_to_bytes(string: &str, width: u8, height: u8) -> Vec<u8> {
     let no_whitespace = string.split_whitespace().collect::<String>();
     let mut bytes = vec![width, height];
+    let mut index;
     for y in 0..(height * 16) as usize {
         for x in 0..width as usize {
-            let index = y * width as usize * 8 + x * 8;
-            let mut sub = no_whitespace
-                .get(index..index + 8)
-                .unwrap_or("00000000")
-                .chars();
-            let mut byte: u8 = 0;
-            for i in 0..8 {
-                if sub.next().unwrap() == '1' {
-                    byte += 0x80 >> i;
-                };
-            }
-            bytes.push(byte);
+            let index = y * width as usize + x;
+            bytes.push(get_byte(&no_whitespace, x, index * 8));
         }
+    }
+    index = width as usize * 8 * height as usize * 16;
+    if no_whitespace.get(index..index + 8 * (1 + 4)).is_some() {
+        for x in 0..4 {
+            bytes.push(get_byte(&no_whitespace, x, index));
+        }
+    }
+    index = index + 8 * 4;
+    while no_whitespace.get(index..index + 8).is_some() {
+        bytes.push(get_byte(&no_whitespace, 0, index));
+        index += 8;
     }
     print!("{bytes:?}");
     bytes
+}
+
+fn get_byte(string: &String, offset: usize, index: usize) -> u8 {
+    let mut sub = string
+        .get(index + 8 * offset..index + 8 * (offset + 1))
+        .unwrap_or("00000000")
+        .chars();
+    let mut byte: u8 = 0;
+    for i in 0..8 {
+        if sub.next().unwrap() == '1' {
+            byte += 0x80 >> i;
+        };
+    }
+    byte
 }
 
 fn to_gf(graphic_bytes: Vec<u8>) {
