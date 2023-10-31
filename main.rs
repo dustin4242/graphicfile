de05373d7e30c8643cbba2d9211c6332433c4bf6
dustin4242 <thegameexposer@gmail.com>
2023-10-31 18:56:13 -0500

    my progress so far to make ppm work

--- main.rs
+++ main.rs
@@ -1,15 +1,18 @@
-use std::{env, fs};
-
 fn main() {
-    //cat ears
-    to_gf(str_to_bytes(
-        &fs::read_to_string(env::args().nth(1).unwrap()).unwrap(),
-        1,
-        1,
-    ));
+    let ppm = std::fs::read_to_string("nyan.ppm").unwrap();
+    let mut no_comment = ppm.split("\n").collect::<Vec<&str>>();
+    if no_comment.get(1).unwrap().starts_with("#") {
+        no_comment.remove(1);
+    };
+    let graphicfile = ppm_to_graphicfile(no_comment.join("\n"));
+    export_gf(graphicfile.unwrap());
 }
 
-fn str_to_bytes(string: &str, width: u8, height: u8) -> Vec<u8> {
+fn export_gf(graphic_bytes: Vec<u8>) {
+    std::fs::write("./gf.gf", graphic_bytes).unwrap();
+}
+
+fn _txt_to_graphicfile(string: &str, width: u8, height: u8) -> Vec<u8> {
     let no_whitespace = string.split_whitespace().collect::<String>();
     // header section
     let mut bytes = vec![width, height];
@@ -18,18 +21,18 @@ fn str_to_bytes(string: &str, width: u8, height: u8) -> Vec<u8> {
     for y in 0..(height * 16) as usize {
         for x in 0..width as usize {
             let index = y * width as usize + x;
-            bytes.push(get_byte(&no_whitespace, x, index * 8));
+            bytes.push(_get_byte(&no_whitespace, x, index * 8));
         }
     }
     // color section
     index = width as usize * 8 * height as usize * 16;
     if no_whitespace.get(index..index + 8 * (1 + 4)).is_some() {
         for x in 0..4 {
-            bytes.push(get_byte(&no_whitespace, x, index));
+            bytes.push(_get_byte(&no_whitespace, x, index));
         }
         index = index + 8 * 4;
         while no_whitespace.get(index..index + 8).is_some() {
-            bytes.push(get_byte(&no_whitespace, 0, index));
+            bytes.push(_get_byte(&no_whitespace, 0, index));
             index += 8;
         }
     }
@@ -37,7 +40,7 @@ fn str_to_bytes(string: &str, width: u8, height: u8) -> Vec<u8> {
     bytes
 }
 
-fn get_byte(string: &String, offset: usize, index: usize) -> u8 {
+fn _get_byte(string: &String, offset: usize, index: usize) -> u8 {
     let mut sub = string
         .get(index + 8 * offset..index + 8 * (offset + 1))
         .unwrap_or("00000000")
@@ -51,6 +54,36 @@ fn get_byte(string: &String, offset: usize, index: usize) -> u8 {
     byte
 }
 
-fn to_gf(graphic_bytes: Vec<u8>) {
-    std::fs::write("./gf.gf", graphic_bytes).unwrap();
+fn ppm_to_graphicfile(ppm: String) -> Option<Vec<u8>> {
+    let mut info = ppm.split_whitespace();
+    info.next();
+    let width = info.next()?.parse::<usize>().unwrap();
+    let height = info.next()?.parse::<usize>().unwrap();
+    println!("{width} {height}");
+    let division: u8 = ((info.next()?.parse::<usize>().unwrap() + 1) / 64) as u8;
+    println!("{division}");
+    let mut image_data = info;
+    let mut converted = Vec::new();
+    for _y in 0..height {
+        for _x in 0..width {
+            let rgb = (
+                image_data.next()?.parse::<u8>().unwrap() / division,
+                image_data.next()?.parse::<u8>().unwrap() / division,
+                image_data.next()?.parse::<u8>().unwrap() / division,
+            );
+            converted.push(rgb);
+        }
+    }
+    let mut pallete: Vec<(u8, u8, u8)> = Vec::with_capacity(8);
+    for x in converted.to_owned() {
+        if !pallete.contains(&x) {
+            pallete.push(x);
+        };
+    }
+    let mut color_information: Vec<u8> = Vec::new();
+    for x in converted {
+        color_information.push(pallete.iter().position(|p| p == &x).unwrap() as u8);
+    }
+    println!("{color_information:?}");
+    Some(vec![])
 }
